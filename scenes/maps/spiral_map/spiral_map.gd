071e69d5fed005cd15646ed5351e2ca862cb235a
Brian Pfretzschner <brian.pfretzschner@gmail.com>
2025-01-20 20:32:37 +0100

    Create Spiral map

---
+++ scenes/maps/spiral_map/spiral_map.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+var game_controller: GameController
+
+func _ready() -> void:
+	assert(game_controller)
+	
+	$CreepPath/Line2d.points = $CreepPath.curve.get_baked_points()
+	add_path_collision_shapes($CreepPath.curve.get_baked_points())
+	
+	game_controller.structure_placed.connect(on_structure_placed)
+	game_controller.creep_spawned.connect(on_creep_spawned)
+
+
+func on_structure_placed(structure: Structure):
+	var structure_scene = preload("res://scenes/structure/structure.tscn").instantiate()
+	structure_scene.game_controller = game_controller
+	structure_scene.structure = structure
+	$Structures.add_child(structure_scene)
+
+
+func on_creep_spawned(creep: Creep):
+	var creep_scence = preload("res://scenes/creep/creep.tscn").instantiate()
+	creep_scence.creep = creep
+	$CreepPath.add_child(creep_scence)
+
+
+func add_path_collision_shapes(points: PackedVector2Array):
+	for i in range(0, points.size() - 1):
+		var p1 = points[i]
+		var p2 = points[i+1]
+		
+		var shape = RectangleShape2D.new()
+		shape.size = (p1 - p2).min(p2 - p1)
+		
+		var collision_shape = CollisionShape2D.new()
+		collision_shape.shape = shape
+		collision_shape.position = p1
+		$PathArea.add_child(collision_shape)
+
+
+func on_map_area_body_exited(body: Node2D) -> void:
+	var creep = body.get_creep()
+	game_controller.creep_passed(creep)
---
+++ tests/map_test.gd
@@ -0,0 +1,24 @@
+extends GutTest
+
+var map_params = [
+	preload("res://scenes/maps/basic_map/basic_map.tscn"),
+	preload("res://scenes/maps/spiral_map/spiral_map.tscn"),
+]
+
+signal timer()
+
+func _before_each(map_scene: PackedScene) -> Node2D:
+	var map = partial_double(map_scene).instantiate()
+	map.game_controller = partial_double(GameController).new(timer)
+	add_child_autofree(map)
+	return map
+
+
+func test_passing_creeps_are_detected(map_scene = use_parameters(map_params)):
+	var map = _before_each(map_scene)
+	
+	var creep = map.game_controller.send_creep()
+	creep.speed *= 100
+	await wait_seconds(0.5)
+
+	assert_called(map.game_controller, "creep_passed", [creep])

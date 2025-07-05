c8eacfd350b4e831fae57da745158b43be382797
Brian Pfretzschner <brian.pfretzschner@gmail.com>
2025-07-05 20:12:56 +0200

    MapCreepBoundary and creep despawning

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use bevy::color::palettes::css::RED;
 use bevy::prelude::*;
+use bevy::color::palettes::css::RED;
+use avian2d::prelude::*;
 use rand;
 use rand::Rng;
 use std::time::Duration;
-use avian2d::PhysicsPlugins;
-use avian2d::prelude::{Collider, CollisionEnded, CollisionEventsEnabled, CollisionStarted};
 
 #[derive(Resource)]
 struct SpawnTimer {
@@ -28,11 +27,20 @@ impl Default for Map {
             .to_curve()
             .unwrap();
 
-        Self {
-            curve,
-        }
+        Self { curve }
     }
 }
+
+#[derive(PhysicsLayer, Default)]
+enum CollisionLayer {
+    #[default]
+    Default, // the default layer that objects are assigned to
+    Creep,
+    MapCreepBoundary,
+}
+
+#[derive(Component)]
+struct MapCreepBoundary {}
 
 #[derive(Component)]
 struct Creep {
@@ -59,7 +67,8 @@ fn main() {
         })
         .init_resource::<Map>()
         .add_systems(Startup, setup_graphics)
-        .add_systems(Update, setup_curve)
+        .add_systems(Startup, setup_map_boundary)
+        .add_systems(Update, map_curve_system)
         .add_systems(Update, spawn_system)
         .add_systems(Update, move_system)
         .add_systems(Update, display_events)
@@ -70,11 +79,29 @@ fn setup_graphics(mut commands: Commands) {
     commands.spawn(Camera2d::default());
 }
 
-fn setup_curve(map: Res<Map>, mut gizmos: Gizmos) {
-    gizmos.linestrip(
-        map.curve.iter_positions(50).map(|pt| pt.extend(0.0)),
-        RED,
-    );
+fn setup_map_boundary(
+	mut commands: Commands,
+	mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+	let mesh = meshes.add(Rectangle::new(
+		1000.0, 500.0
+	));
+    let color = Color::hsl(12., 0.15, 0.2);
+    let material = materials.add(color);
+
+    commands.spawn((
+		MapCreepBoundary {},
+		Mesh2d(mesh),
+        MeshMaterial2d(material),
+		Collider::rectangle(1000.0, 500.0),
+		CollisionEventsEnabled::default(),
+		CollisionLayers::new(CollisionLayer::MapCreepBoundary, [CollisionLayer::Creep]),
+	));
+}
+
+fn map_curve_system(map: Res<Map>, mut gizmos: Gizmos) {
+    gizmos.linestrip(map.curve.iter_positions(50).map(|pt| pt.extend(0.0)), RED);
 }
 
 fn spawn_system(
@@ -103,7 +130,7 @@ fn move_system(
         path_follow.progress += velocity * time.delta_secs();
         transform.translation = map.curve.position(path_follow.progress).extend(0.0);
     }
-}
+}	
 
 fn creep_bundle(
     mut meshes: ResMut<Assets<Mesh>>,
@@ -112,7 +139,7 @@ fn creep_bundle(
 ) -> impl Bundle {
     let mut rng = rand::rng();
 
-    let speed = rng.random_range(10. .. 500.);
+    let speed = rng.random_range(10. ..500.);
     let radius = 20.;
 
     let mesh = meshes.add(Circle::new(radius));
@@ -125,20 +152,17 @@ fn creep_bundle(
         Mesh2d(mesh),
         MeshMaterial2d(material),
         Transform::from_translation(initial_position.extend(0.0)),
-
         Collider::circle(radius),
-        CollisionEventsEnabled::default(),
+		CollisionEventsEnabled::default(),
+		CollisionLayers::new(CollisionLayer::Creep, [CollisionLayer::MapCreepBoundary]),
     )
 }
 
 fn display_events(
-    mut collision_started_events: EventReader<CollisionStarted>,
+	mut commands: Commands,
     mut collision_ended_events: EventReader<CollisionEnded>,
 ) {
-    for collision_event in collision_started_events.read() {
-        println!("  Received collision start event: {:?}", collision_event);
-    }
     for collision_event in collision_ended_events.read() {
-        println!("X Received collision ended event: {:?}", collision_event);
+		commands.entity(collision_event.0).despawn();
     }
 }
